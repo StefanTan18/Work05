26487846d1d38ef27e03c64afd47202da2369098
Trollicorn <muddin3@stuy.edu>
2018-09-28 11:02:09 -0400

    added strcmp

--- temp.c
+++ temp.c
@@ -7,3 +7,25 @@ char * strncpy( char *dest, char *source, int n ){
 	}
 	return dest;
 }
+
+int strcmp( char *s1, char *s2 ){
+	int len1 = strlen( *s1); 
+	int len2 = strlen( *s2);
+	int n;
+	if (len1 > len2){
+		n = len1;
+	}
+	else{
+		n = len2;
+	}
+	for (int i = 0; i < n; i++){
+		if (s1[i] > s2[i]){
+			return 1;
+		}
+		if (s1[i] < s2[i]){
+			return =1;
+		}
+	return 0;
+	}
+
+}
