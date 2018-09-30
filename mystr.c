a922735ed808bc802feb7cd862b359baa7c682a1
Stefan Tan <stan3@stuy.edu>
2018-09-30 14:47:58 -0400

    merged mystr.c and temp.c

--- mystr.c
+++ mystr.c
@@ -7,12 +7,40 @@ int strlen( char * str ) {
   return i;
 }
 
+char * strncpy( char *dest, char *source, int n ){
+  for (int i = 0; i < n && source[i]; i++){
+    dest[i] = source[i];
+  }
+  return dest;
+}
+
 char * strcat( char *dest, char *source ) {
   int len = strlen(*dest);
   for (i = 0; i < strlen(*source); i++)
     dest[len + i] = str[i];
   dest[len + str(*source)] = '\0';
   return dest;
+}
+
+int strcmp( char *s1, char *s2 ){
+  int len1 = strlen( *s1); 
+  int len2 = strlen( *s2);
+  int n;
+  if (len1 > len2){
+    n = len1;
+  }
+  else{
+    n = len2;
+  }
+  for (int i = 0; i < n; i++){
+    if (s1[i] > s2[i]){
+      return 1;
+    }
+    if (s1[i] < s2[i]){
+      return -1;
+    }
+    return 0;
+  }
 }
   
 char * strchr( char *s, char c ) {
